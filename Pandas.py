cc7213248c688369694bc50bb7da2ea710a78257
Anton <anton.naumenko.a@gmail.com>
2025-01-31 20:22:01 +0100

    visualization

--- Pandas.py
+++ Pandas.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
 import math as mt
+import matplotlib.pyplot as plt
+
 car_data = pd.read_csv(r'car_price_dataset.csv')
+
+car_data_audi = car_data[car_data['Brand'] == 'Audi']
 
 engine_size_subset = car_data[['Engine_Size']]
 
@@ -13,12 +17,19 @@ column_year = car_data[['Year']]
 column_engine_size = car_data[['Engine_Size']]
 column_fuel_type = car_data[['Fuel_Type']]
 column_transmition = car_data[['Transmission']]
-column_mileage = car_data[['Mileage']]
+
 column_doors = car_data[['Doors']]
 
 #separated_columns_Pandas_Series
 column_owner_count = car_data['Owner_Count']
 column_price = car_data['Price']
+column_mileage = car_data['Mileage']
+
+
+#separated_columns_Pandas_Series_Audi
+column_owner_count_audi = car_data_audi['Owner_Count']
+column_price_audi = car_data_audi['Price']
+column_mileage_audi = car_data_audi['Mileage']
 
 #creating arrays 
 np_column_price = np.array(column_price)
@@ -37,7 +48,18 @@ price_std = np_column_price.std()  # Standard deviation
 
 #print(car_data.loc[4])
 
+#Visualization of data
+plt.scatter(column_mileage_audi,column_price_audi, s=10,cmap='coolwarm',zorder=1)
 
 
+
+plt.yscale('linear')  # Options: 'linear', 'log', 'symlog', 'logit'
+plt.xscale('linear')  # Options: 'linear', 'log', 'symlog', 'logit'
+
+plt.xlabel("Milage", fontsize=12)
+plt.ylabel("Price", fontsize=12)
+plt.title("Audi price to mileage", fontsize=14)
+
+plt.show()
 #outputs
 print('Standard deviation of car price is: ', round(price_std))

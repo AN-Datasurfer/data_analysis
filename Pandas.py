0897bee5dd9ee8aab9992fe3d7fb9065308ea8db
Anton <anton.naumenko.a@gmail.com>
2025-01-31 17:52:55 +0100

    Commit

--- Pandas.py
+++ Pandas.py
@@ -1,3 +1,9 @@
 import pandas as pd
 car_data = pd.read_csv(r'car_price_dataset.csv')
-print(car_data.info)
+
+engine_size_subset = car_data[['Engine_Size']]
+
+#print(car_data.info)
+print(engine_size_subset.info)
+
+print(car_data.loc[4])

4fae775fbc736f6f39b70d1bac814b55cc36d6b2
Anton <anton.naumenko.a@gmail.com>
2025-02-03 21:12:01 +0100

    python_random

--- python_car_price_analysis.py
+++ python_car_price_analysis.py
@@ -4,11 +4,12 @@ import math as mt
 import matplotlib.pyplot as plt
 
 car_data = pd.read_csv(r'car_price_dataset.csv')
-
 car_data_audi = car_data[car_data['Brand'] == 'Audi']
 
-engine_size_subset = car_data[['Engine_Size']]
+#variables
+analised_mileage = 10000
 
+car_data_audi_milage = car_data_audi[car_data_audi['Mileage'] < analised_mileage]
 
 #separated_columns_Pandas_DataFrame
 column_brand = car_data[['Brand']]
@@ -17,7 +18,6 @@ column_year = car_data[['Year']]
 column_engine_size = car_data[['Engine_Size']]
 column_fuel_type = car_data[['Fuel_Type']]
 column_transmition = car_data[['Transmission']]
-
 column_doors = car_data[['Doors']]
 
 #separated_columns_Pandas_Series
@@ -27,39 +27,39 @@ column_mileage = car_data['Mileage']
 
 
 #separated_columns_Pandas_Series_Audi
-column_owner_count_audi = car_data_audi['Owner_Count']
-column_price_audi = car_data_audi['Price']
-column_mileage_audi = car_data_audi['Mileage']
+column_owner_count_audi = car_data_audi_milage['Owner_Count']
+column_price_audi = car_data_audi_milage['Price']
+column_mileage_audi = car_data_audi_milage['Mileage']
+column_model_audi = car_data_audi_milage['Model']
 
 #creating arrays 
 np_column_price = np.array(column_price)
 np_column_price_to_owner_count = np.array(column_price, column_owner_count)
 
 #price_descriptive_statistics
-price_sum = np_column_price.sum() # Sum of elements
+price_sum = np_column_price.sum()    # Sum of elements
 price_mean = np_column_price.mean()  # Mean of elements
-price_min = np_column_price.min()  # Minimum value
-price_max = np_column_price.max()  # Maximum value
-price_std = np_column_price.std()  # Standard deviation
+price_min = np_column_price.min()    # Minimum value
+price_max = np_column_price.max()    # Maximum value
+price_std = np_column_price.std()    # Standard deviation
 
-
-#print(car_data.info)
-#print(engine_size_subset.info)
-
-#print(car_data.loc[4])
+#Variables for viz
+x = column_price_audi
+y = column_owner_count_audi
 
 #Visualization of data
-plt.scatter(column_mileage_audi,column_price_audi, s=10,cmap='coolwarm',zorder=1)
+plt.scatter(x ,y, s=10,cmap='coolwarm',zorder=1)
 
-
-
-plt.yscale('linear')  # Options: 'linear', 'log', 'symlog', 'logit'
-plt.xscale('linear')  # Options: 'linear', 'log', 'symlog', 'logit'
+plt.yscale('linear')      # Options: 'linear', 'log', 'symlog', 'logit'
+plt.xscale('linear')      # Options: 'linear', 'log', 'symlog', 'logit'
 
 plt.xlabel("Milage", fontsize=12)
 plt.ylabel("Price", fontsize=12)
 plt.title("Audi price to mileage", fontsize=14)
 
 plt.show()
+
+
+
 #outputs
 print('Standard deviation of car price is: ', round(price_std))

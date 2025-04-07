8c0dd015b504aa407dc26609c43bedc5924e4ebb
Anton <anton.naumenko.a@gmail.com>
2025-04-07 17:50:27 +0200

    Fix variable naming for results in output display

--- functions.py
+++ functions.py
@@ -62,14 +62,14 @@ def ex_3(number_2):
     number_x3 = number_2 *3
     return number_x3
 
-result_2 = ex_2(my_number)
-result_3 = ex_3(my_number)
+result_ex2 = ex_2(my_number)
+result_ex3 = ex_3(my_number)
 
 output_window = tk.Toplevel()
 output_window.title("Results")
 output_window.geometry("300x200")  # Set the size of the output window (width x height)
 
-output_label = tk.Label(output_window, text=f"Result of ex_2: {result_2}\nResult of ex_3: {result_3}")
+output_label = tk.Label(output_window, text=f"Result of ex_2: {result_ex2}\nResult of ex_3: {result_ex3}")
 output_label.pack(pady=20)  # Add some padding for better layout
 
 output_window.mainloop()

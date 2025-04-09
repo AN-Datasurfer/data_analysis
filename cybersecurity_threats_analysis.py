a8b805e4491385529ca4a37127dc74ed67ee0f55
Anton <anton.naumenko.a@gmail.com>
2025-04-09 16:16:33 +0200

    Refactor financial loss analysis variables for clarity and consistency

--- cybersecurity_threats_analysis.py
+++ cybersecurity_threats_analysis.py
@@ -9,8 +9,12 @@ data = pd.read_csv(r'Datasets\Global_Cybersecurity_Threats_2015-2024.csv')
 
 # Financial loss analysis
 financial_loss_by_year = data.groupby('Year')[["Financial Loss (in Million $)"]].sum().sort_values(by='Financial Loss (in Million $)', ascending=False)
-avg_financial_loss_by_year = data.groupby('Year')[["Financial Loss (in Million $)"]].mean().sort_values(by='Financial Loss (in Million $)', ascending=False)
+financial_loss_by_year_avg = data.groupby('Year')[["Financial Loss (in Million $)"]].mean().sort_values(by='Financial Loss (in Million $)', ascending=False)
+financial_loss_by_year_max = data.groupby('Year')[["Financial Loss (in Million $)"]].max().sort_values(by='Year', ascending=False)
+financial_loss_by_year_min = data.groupby('Year')[["Financial Loss (in Million $)"]].min().sort_values(by='Year', ascending=False)
 
 print(financial_loss_by_year)
-print(avg_financial_loss_by_year)
+print(financial_loss_by_year_avg)
+print(financial_loss_by_year_max)
+print(financial_loss_by_year_min)
 
